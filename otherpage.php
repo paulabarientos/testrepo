9181bece1c56b00aa5a27ac6e69716646d1c0c8e
paulabarientos <paul.abarientos@msl.com.ph>
2026-08-13 16:52:03 +0800

    added files

---
+++ otherpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Sample Web Page</title>
+</head>
+<body>
+
+    <!-- Main Heading -->
+    <h1>Welcome to Other Page</h1>
+    
+
+</body>
+</html>
+
+
